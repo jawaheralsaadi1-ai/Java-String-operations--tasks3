33a17c6d564d695e79971b160da71571d2f374fb
Jawaher <8alsaadii@gmail.com>
2026-04-22 16:43:40 +0400

    Add Task 7: Same End

---
+++ src/Task6SameEnd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task6SameEnd {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in); // Set up the tool to read input from user
+        while (true) {
+            System.out.print("Enter text : ");// Ask user to Enter text
+            String text = inputScanner.nextLine(); // Save Text Input
+
+        int digitSum = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char currentChar = text.charAt(i);
+
+            // Check if the character is a digit '0' through '9'
+            if (Character.isDigit(currentChar)) {
+                // Convert char to string, then parse it to an integer
+                digitSum += Integer.parseInt(String.valueOf(currentChar));
+            }
+        }
+        //Display Result
+        System.out.println("Sum of digits: " + digitSum);
+    }}}
